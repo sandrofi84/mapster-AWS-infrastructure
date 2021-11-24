e711d40e71041c8b024ecc6217bd656ae9a7a77a
Sandro Fillinich <me@sandrofi.com>
2021-11-24 17:17:17 +0000

    feat: add aws-cdk dependencies for s3 and cloudfront

--- lib/mapster-AWS-infrastructure.ts
+++ lib/mapster-AWS-infrastructure.ts
@@ -1,15 +1,32 @@
-import * as cdk from "@aws-cdk/core";
-import * as sqs from "@aws-cdk/aws-s3";
+import * as cdk from '@aws-cdk/core'
+import * as s3 from '@aws-cdk/aws-s3'
+import * as cloudfront from '@aws-cdk/aws-cloudfront'
+import { S3Origin } from '@aws-cdk/aws-cloudfront-origins'
 
 export class MapsterAWSInfrastructureStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
 
-    // The code that defines your stack goes here
+        // S3
+        const devBucket = new s3.Bucket(this, 'mapsterBucketDevelopment', {
+            bucketName: 'mapster-bucket-development',
+            publicReadAccess: true,
+            websiteIndexDocument: 'index.html',
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+        })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureForAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // CloudFront
+        const distribution = new cloudfront.Distribution(
+            this,
+            'mapsterDistribution',
+            {
+                defaultBehavior: { origin: new S3Origin(devBucket) },
+            }
+        )
+
+        // example resource
+        // const queue = new sqs.Queue(this, 'InfrastructureForAwsQueue', {
+        //   visibilityTimeout: cdk.Duration.seconds(300)
+        // });
+    }
 }
