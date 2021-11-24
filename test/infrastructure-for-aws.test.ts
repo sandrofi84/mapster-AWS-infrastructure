87f7aa0c30068550ee50b1ea40acea1bec9937fb
Sandro Fillinich <me@sandrofi.com>
2021-11-24 16:44:34 +0000

    feat: add .nvmrc file

---
+++ test/infrastructure-for-aws.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as InfrastructureForAws from '../lib/infrastructure-for-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infrastructure-for-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfrastructureForAws.InfrastructureForAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
