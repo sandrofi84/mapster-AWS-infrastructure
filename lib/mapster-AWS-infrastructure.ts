4f400ff58d0780411727279159168d248a88aa28
Sandro Fillinich <me@sandrofi.com>
2021-11-29 17:57:52 +0000

    feat: add access policy based on principalTag to buckets

--- lib/mapster-AWS-infrastructure.ts
+++ lib/mapster-AWS-infrastructure.ts
@@ -1,4 +1,5 @@
 import * as cdk from '@aws-cdk/core'
+import * as iam from '@aws-cdk/aws-iam'
 import * as s3 from '@aws-cdk/aws-s3'
 import * as cloudfront from '@aws-cdk/aws-cloudfront'
 import { S3Origin } from '@aws-cdk/aws-cloudfront-origins'
@@ -15,11 +16,14 @@ export class MapsterAWSInfrastructureStack extends cdk.Stack {
         props: MapsterAWSInfrastructureStackProps
     ) {
         super(scope, id, props)
-
-        const isProd = props.deploymentEnvironment === 'prod'
+        const environment = props.deploymentEnvironment
+        const isProd = environment === 'prod'
         const bucketId = isProd
             ? 'mapsterBucketProduction'
             : 'mapsterBucketDevelopment'
+        const bucketPolicyId = isProd
+            ? 'mapsterBucketPolicyProduction'
+            : 'mapsterBucketPolicyDevelopment'
         const bucketProps: s3.BucketProps = {
             bucketName: isProd
                 ? 'mapster-bucket-production'
@@ -28,8 +32,25 @@ export class MapsterAWSInfrastructureStack extends cdk.Stack {
             websiteIndexDocument: 'index.html',
             removalPolicy: cdk.RemovalPolicy.RETAIN,
         }
-        // S3
+        // create S3 bucket
         const bucket = new s3.Bucket(this, bucketId, bucketProps)
+
+        // create the bucket policy
+        const bucketPolicy = new s3.BucketPolicy(this, bucketPolicyId, {
+            bucket: bucket,
+        })
+
+        // add policy statements ot the bucket policy
+        bucketPolicy.document.addStatements(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ['s3:GetObject', 's3:DeleteObject', 's3:PutObject'],
+                resources: [`${bucket.bucketArn}/*`],
+                conditions: {
+                    StringEquals: { 'aws:PrincipalTag/env': environment },
+                },
+            })
+        )
 
         if (isProd) {
             // CloudFront
@@ -37,10 +58,5 @@ export class MapsterAWSInfrastructureStack extends cdk.Stack {
                 defaultBehavior: { origin: new S3Origin(bucket) },
             })
         }
-
-        // example resource
-        // const queue = new sqs.Queue(this, 'InfrastructureForAwsQueue', {
-        //   visibilityTimeout: cdk.Duration.seconds(300)
-        // });
     }
 }
